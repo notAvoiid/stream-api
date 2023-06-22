11358de5e77a184a4c83246745f99b407e79ec5c
Igor <igor.nacimento51@gmail.com>
2023-06-21 22:47:01 -0300

    "Streams: First ideas with streams"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> list = Arrays.asList(3,4,5,10,7);
+
+        Stream<Integer> st1 = list.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(st1.toArray()));
     }
 }
