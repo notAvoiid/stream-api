f11807712dec2af38a8b7ee6798a6ba097289d9d
Igor <igor.nacimento51@gmail.com>
2023-06-21 22:50:48 -0300

    "Streams: Iterating the streams"

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,11 @@ public class Main {
 
         Stream<Integer> st1 = list.stream().map(x -> x * 10);
         System.out.println(Arrays.toString(st1.toArray()));
+
+        Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+        System.out.println(Arrays.toString(st2.toArray()));
+
+        Stream<Integer> st3 = Stream.iterate(0, x -> x+2);
+        System.out.println(Arrays.toString(st3.limit(10).toArray()));
     }
 }
