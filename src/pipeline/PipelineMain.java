8b875ade843f71e44fc9bfe7e2efc1d1ff351cae
Igor <igor.nacimento51@gmail.com>
2023-06-26 23:48:13 -0300

    "Streams: Exercise with pipelines and streams"

--- src/pipeline/PipelineMain.java
+++ src/pipeline/PipelineMain.java
@@ -2,10 +2,9 @@ package pipeline;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Main {
+public class PipelineMain {
     public static void main(String[] args) {
 
         List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
